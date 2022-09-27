119cb9a6294f62a49e65335918034192951b8656
[TheSimBiOne] <[thesimbione@gmail.com]>
2022-09-27 23:00:11 +0300

    Обновил репозиторий к лекции 28.09.022

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('hello, world!')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Подключение модулей
+import math
+import numpy
+import matplotlib.pyplot as mpp
+
+# Эта программа рисует график функции, заданной выражением ниже
+
+if __name__ == '__main__':  # Тело условия не выполнится в случае,
+    # если файл будет импортирован
+    arguments = numpy.arange(0, 200, 0.1)  # Создание массива аргументов
+    mpp.plot(
+        arguments,
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+        # Создание объекта типа "график"
+    )
+    mpp.show()  # Вывод графика на экран
