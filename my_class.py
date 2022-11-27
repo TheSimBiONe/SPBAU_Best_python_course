035e5221c248101ca2c12b77e7f455588f335d61
[TheSimBiOne] <[thesimbione@gmail.com]>
2022-11-27 21:32:38 +0300

    Обновил репозиторий к лекции 23.11.2022 Добавил программу с классов Universe_2D и проверку закона Кеплера. Так же добавил свой класс, но он пока не доделан

---
+++ my_class.py
@@ -0,0 +1,164 @@
+# Далее под перестановкой я имею ввиду сокращённою табличную запись перестановки
+# А под элементом симметрической группы - собственно перестановку
+
+
+def cycle_from_permutation(σ, begin, cycle):
+    '''
+    Находит цикл из σ в который входит элемент begin.
+
+    :param σ: Перестановка
+    :param begin: Один из элементов
+    :param cycle: Нужна для возврата цикла
+    :return:
+    '''
+    if begin in cycle:
+        return begin
+    cycle.append(begin)
+    cycle_from_permutation(σ, σ[begin - 1], cycle)
+    return cycle
+
+
+class Permutation:
+    def __init__(self, permutation=[]):
+        '''
+        Класс элемента симметрической группы, задаётся в виде перестановки или произведения
+        непересекающихся циклов. Если используется последний способ, то в конце нужно указать
+        длинну перестановки. Если не передать аргументов, объект будет считаться тождественной
+        перестановкой.
+
+        :param permutation: [a, b, c, .., d] или ['(a b .. c)(d e .. f)..(g h .. i)',  кол-во эл]
+        '''
+
+        self.cycles = []  # Разложение перестановки в циклы
+        self.permutation = []  # Перестановка
+        self.id = False  # С тождестенной перестановкой будем работать подругому
+
+        try:
+            if len(permutation) == 0:
+                self.id = True
+
+            elif type(permutation[0]) == int:  # Если мы передаём как перестановку, то находим циклы
+                self.permutation = list(permutation)
+                self.solve_cycles()
+                self.id = self.is_id()
+
+            elif type(permutation[0]) == str:  # Если мы передаём как циклы, то находим перестановку
+                self.cycles = [list(map(int, cycle[:-1].split())) for cycle in permutation[0].split('(')[1:]]
+                self.permutation = [i + 1 for i in range(permutation[-1])]
+                self.solve_permutation()
+                self.id = self.is_id()
+        except IndexError:
+            pass
+
+    def solve_cycles(self):
+        '''
+        Находит на основании self.permutation разложение в циклы для self.cycles
+        '''
+
+        permutation1 = self.permutation.copy()
+        self.cycles.clear()
+        while permutation1:
+            self.cycles.append(cycle_from_permutation(self.permutation, permutation1[0], []))
+            for i in self.cycles[-1]:
+                permutation1.remove(i)  # Удаление элемента, кототрый уже есть в одном из циклов
+
+    def solve_permutation(self):
+        '''
+        Находит на основании self.cycles перестановку для self.permutation
+        '''
+
+        for cycle in reversed(self.cycles):
+            l = len(cycle)
+            for i in range(l):
+                self.permutation[cycle[i] - 1] = cycle[(i + 1) % l]
+
+    def __str__(self):
+        '''
+        Печатает в консоль перестановку если вызвать print.
+        '''
+
+        if self.id:
+            return 'id'
+        return f"({', '.join(list(map(str, tuple(self.permutation))))})"
+
+    def print_cycles(self):
+        '''
+        Печатает в консоль произведение непересекющихся циклов.
+        '''
+
+        if self.id:
+            print('id')
+        else:
+            print(''.join([f"({' '.join(list(map(str, cycle)))})" for cycle in self.cycles if len(cycle) > 1]))
+
+    def is_id(self):
+        '''
+        :return: True если перестановка тождественная, False если нет. Атрибут self.id хранит
+        аналогичное значение.
+        '''
+
+        for i in range(len(self.permutation)):
+            if i + 1 != self.permutation[i]:
+                return False
+
+        self.permutation = [1]
+        self.cycles = [1]
+
+        return True
+
+    def __eq__(self, other):
+        try:
+            if self.permutation == other.permutation:
+                return True
+        except AttributeError:  # Если сравниваем с объектом не класса Permutation, то вернём False
+            return False
+        return False
+
+    def __mul__(self, other):
+        '''
+        Метод считает композицию двух элементов симметрической группы.
+        Как и в алгебре первая применяется перестановка, которая справа.
+
+        :param other: Экземпляр класса Permutation
+        :return: Композиция двух элементов симметической подгруппы
+        '''
+
+        if type(other) != Permutation:
+            raise TypeError("unsupported operand type(s) for *: 'Permutation' and 'int'")
+
+        if self.id:
+            return Permutation(other.permutation)
+        elif other.id:
+            return Permutation(self.permutation)
+
+        result = []
+        l = max(len(self.permutation), len(other.permutation))
+
+        # Исключения нужны, чтобы обработать умножение перестановок, которые могут не лежать в одной
+        # симметрической группе.
+        for i in range(1, l + 1):
+            try:
+                result.append(self.permutation[other.permutation[i - 1] - 1])
+            except IndexError:
+                try:
+                    result.append(other.permutation[i - 1])
+                except IndexError:
+                    result.append(self.permutation[i - 1])
+
+        return Permutation(result)
+
+    def __neg__(self):
+        '''
+        :return: Обратный элемент к элементу симметрической группы.
+        '''
+
+        result = []
+        for i in range(1, len(self.permutation) + 1):
+            result.append(self.permutation.index(i) + 1)
+
+        return Permutation(result)
+
+
+a = Permutation([4, 3, 1, 2, 6, 7, 5])
+b = Permutation([3, 2, 4, 5, 1])
+c = Permutation([6, 3, 2, 5, 4, 1])
