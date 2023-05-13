6398433464e9b38128655bb891d8f611e02f498a
[TheSimBiOne] <[thesimbione@gmail.com]>
2023-05-14 02:14:31 +0300

    Приложение на flask

--- parallel_boost.py
+++ parallel_boost.py
@@ -1,21 +1,34 @@
 import multiprocessing
 import random
-import math
+from math import e, pi
+from numpy import sin, cos, tan, log, exp
+from numpy import linspace
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+matplotlib.use('agg')
+
+bc = mcolors.BASE_COLORS
 
 
-def monte_carlo(func, points, x_limits, y_limits, accuracy=1):
+def monte_carlo(func, points, x_limits, y_limits, accuracy=100000):
     '''
 
-    :param func: интегрируемая функция
-    :param points: количество точек для посчёта
+    :param func: интегрируемая функция (подаётся в виде строки);
+                 примеры: 'sin(x)' 'x ** 2'
+    :param points: количество точек для подсчёта
     :param x_limits: переделы интегрирования
     :param y_limits: допустимые значения
-    :param accuracy: в случае если значения будут добными нужно указать, насколько нужно умножить
+    :param accuracy: в случае если значения будут дробными нужно указать, насколько нужно умножить
                      чтобы получились целые
-    :return:
+    :return: значение, множество иксов, множество игриков, множество цветов
     '''
 
     square = (x_limits[1] - x_limits[0]) * (y_limits[1] - y_limits[0])
+
+    points_plot_x = []
+    points_plot_y = []
+    points_plot_color = []
 
     top_right_points = 0
     bottom_right_points = 0
@@ -26,24 +39,64 @@ def monte_carlo(func, points, x_limits, y_limits, accuracy=1):
         x = random.randrange(x_limits[0], x_limits[1]) / accuracy
         y = random.randrange(y_limits[0], y_limits[1]) / accuracy
 
-        a = func(x)
+        a = eval(f'{func}')
+        color = 'b'
+
         if a >= 0 and y >= 0:
             if a >= y:
                 top_right_points += 1
+                color = 'r'
         elif a < 0 and y < 0:
             if a <= y:
                 bottom_right_points += 1
+                color = 'r'
 
-    return (top_right_points - bottom_right_points) / points * square
+        points_plot_x.append(x)
+        points_plot_y.append(y)
+        points_plot_color.append(color)
+
+    return (top_right_points - bottom_right_points) / points * square, \
+           points_plot_x, \
+           points_plot_y, \
+           points_plot_color
 
 
 def parallel_monte_carlo(pool, func, points, x_calc, y_calc, accuracy, tests=10):
     args = [(func, points, x_calc, y_calc, accuracy) for _ in range(tests)]
-    return sum(pool.starmap(monte_carlo, args)) / tests
+    values = [i[0] for i in pool.starmap(monte_carlo, args)]
+    return sum(values) / tests
+
+
+def build_plot(func, bot_top, x, y, colors):
+
+    '''
+    Функция строит график по результатам работы функции monte_carlo
+
+    :param func: функция для построения
+    :param bot_top: пределы построения функции по иксу
+    :param x: множество иксов (результат работы monte_carlo)
+    :param y: множество игриков (результат работы monte_carlo)
+    :param colors: множество цветов (результат работы monte_carlo)
+    :return:
+    '''
+
+    # with используется чтобы сделать оси графика и цифры на нихбеленьким
+    with plt.rc_context({'axes.edgecolor': 'white', 'xtick.color': 'white', 'ytick.color': 'white'}):
+        ax = plt.axes()
+        ax.grid(which='major', alpha=1)
+
+        plt.scatter(x, y, s=1, c=colors)
+
+        x = linspace(bot_top[0], bot_top[1])
+        plt.plot(x, eval(f'{func}'), color='green')
+
+        # plt.show()
+        plt.savefig('static/plot.png', dpi=500, bbox_inches='tight', transparent=True)
+
+        # Очищение графика для следующего интеграла
+        plt.clf()
 
 
 if __name__ == '__main__':
     poool = multiprocessing.Pool()
-    print(parallel_monte_carlo(poool, math.sin, 1000000, (0, 2 * 3.1415), (-1, 1), 10000))
-else:
-    print("__name__:", __name__)
+    print(parallel_monte_carlo(poool, 'sin(x)', 1000000, (0, 2 * 3.1415), (-1, 1), 10000))
---
+++ web_app.py
@@ -0,0 +1,36 @@
+import flask
+from flask import Flask, request
+from parallel_boost import monte_carlo, build_plot
+
+
+app = Flask(__name__, static_folder="static", static_url_path="", template_folder="templates")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    if request.method == 'GET':
+        return flask.render_template(
+            'base.html')
+    elif request.method == 'POST':
+
+        result = monte_carlo(request.form.get('func'),
+                             int(request.form.get('points')),
+                             tuple(map(float, request.form.get('x_limits').split())),
+                             tuple(map(float, request.form.get('y_limits').split())))
+
+        build_plot(request.form.get('func'),
+                   tuple(map(float, request.form.get('x_limits').split())),
+                   *result[1:])
+
+        return flask.render_template(
+            'index.html',
+            func=request.form.get('func'),
+            points=request.form.get('points'),
+            x_limits=request.form.get('x_limits'),
+            y_limits=request.form.get('y_limits'),
+            value=result[0],
+            method=request.method)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
